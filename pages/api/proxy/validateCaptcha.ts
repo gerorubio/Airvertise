d66001b055770e96372a6a170915c22d136ac23e
Faustino Gagneten <fgagneten@gmail.com>
2022-10-11 17:30:44 -0300

    Add missing "s"

--- pages/api/proxy/validateCaptcha.ts
+++ pages/api/proxy/validateCaptcha.ts
@@ -7,7 +7,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { recaptchaToken } = req.body
 
-    // Rewrite the URL to verify with Google if the captcha is valid (The token is valid only for 2 minutes)
+    // Rewrites the URL to verify with Google if the captcha is valid (The token is valid only for 2 minutes)
     req.url = "https://www.google.com/recaptcha/api/siteverify"
     req.body = `secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${recaptchaToken}`
     const config = {

7468f636cd7e17fb51d8ec18787dbcecf73d4a23
Sorpresa <gerorubio@outlook.com>
2022-11-30 12:01:02 -0600

    i18next config for vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de'],
-    localePath: path.resolve('./public/static/locales')
+    locales: ['en', 'es'],
+    localePath: path.resolve('./public/locales')
   }
 }

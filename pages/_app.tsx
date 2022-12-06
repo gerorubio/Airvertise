bd4e017ee96b0510005f8b974122e48e29853e54
Sorpresa <gerorubio@outlook.com>
2022-12-06 14:51:17 -0600

    Bgcolor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -64,10 +64,12 @@ function WebApp(props: AirvertiseProps) {
                 >
                     <ThemeProvider theme={lightTheme}>
                         <CssBaseline />
-                        <>
+                        <div style={{
+                            background: "linear-gradient(#FE7B26, #FB145E, #010101);"
+                        }}>
                             <Component {...pageProps} />
                             <Web3Modal config={modalConfig} />
-                        </>
+                        </div>
                     </ThemeProvider>
                 </GoogleReCaptchaProvider>
             </RootStoreProvider>
